c8144d011f9da91ab549b632ac8aee8bf6ead1af
Jeffrey Bullock <thetanbull@gmail.com>
2018-06-13 12:54:17 -0400

    Randomized Colors
    Added randomizer function to roll rgb colors

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,4 @@
-var colors = [
-    "rgb(255, 0, 0)", 
-    "rgb(255, 255, 0)", 
-    "rgb(0, 255, 0)", 
-    "rgb(0, 255, 255)", 
-    "rgb(0, 0, 255)", 
-    "rgb(255, 0, 255)", 
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -37,6 +30,27 @@ function changeColors(color){
 }
 
 function pickColor(){
-   var random = Math.floor(Math.random() * colors.length + 1);
+   var random = Math.floor(Math.random() * colors.length);
+   console.log(random);
+   console.log(colors[random]);
    return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    //pick a "red" from 0-255
+    var r = Math.floor(Math.random() * 256)
+    //pick a "green" from 0-255
+    var g = Math.floor(Math.random() * 256)
+    //pick a "blue" from 0-255
+    var b = Math.floor(Math.random() * 256)
+    "rgb(r, g, b)"
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }

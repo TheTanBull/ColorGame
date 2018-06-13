a378268ab394cff39acb31e4d86f55913f15e0f5
Jeffrey Bullock <thetanbull@gmail.com>
2018-06-13 12:29:42 -0400

    Randomized Winning Color
    Added a randomizer for the winning color

--- colorGame.js
+++ colorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,10 +21,22 @@ for(var i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", function(){
         var clickedColor = this.style.backgroundColor;
         if(clickedColor === pickedColor){
-            alert("Correct!");
+            messageDisplay.textContent = "Correct!";
+            changeColors(pickedColor);
         } else {
-            alert("Wrong");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
 }
 
+function changeColors(color){
+    for(var i = 0; i < colors.length; i++){        
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+   var random = Math.floor(Math.random() * colors.length + 1);
+   return colors[random];
+}
